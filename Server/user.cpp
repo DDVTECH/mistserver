d94140c660f9150cca67b26f8bf5724ee8a27c10
Thulinma <thulinma@localhost.localdomain>
2010-06-21 01:08:26 +0200

    Ringbuffer werkend. Denk ik. Testing needed. Also - commentaar toegevoegd en zo

--- Server/user.cpp
+++ Server/user.cpp
@@ -1,36 +1,51 @@
 #include "user.h"
 
 user::user() {
-  myBuffer = NULL;
-  myConnection = NULL;
+  Conn = NULL;
+  is_connected = false;
 }
 
 user::~user() {
-  myConnection->disconnect();
-  myConnection = NULL;
-}
-
-void user::set_buffer(buffer * newBuffer) {
-  myBuffer = newBuffer;
-  sent = 0;
-}
-
-int user::get_number() { return myBuffer->number; }
-
-bool user::complete_send() {
-  if (sent == myBuffer->size) { return true; }
-  return false;
+  Conn->disconnect();
+  Conn = NULL;
+  is_connected = false;
 }
 
 void user::disconnect() {
-  if (myConnection) {
-    myConnection->disconnect();
-    myConnection = NULL;
+  if (Conn) {
+    Conn->disconnect();
+    Conn = NULL;
   }
+  std::cout << "Disconnected user" << std::endl;
+  is_connected = false;
+}
+void user::connect(SWBaseSocket * newConn) {
+  Conn = (SWUnixSocket*)newConn;
+  is_connected = (Conn != 0);
 }
 
-void user::connect(SWUnixSocket * newConnection) { myConnection = newConnection; }
-
-bool user::is_connected( ) { return myConnection; }
-
-int user::send_msg(char * message, int length, SWBaseSocket::SWBaseError * BError) { return myConnection->send(message,length,BError); }
+void user::Send(buffer ** ringbuf, int buffers){
+  //not connected? cancel
+  if (!is_connected){return;}
+  //still waiting for next buffer? check it
+  if (MyBuffer_num < 0){MyBuffer_num = ringbuf[MyBuffer]->number;}
+  //still waiting? don't crash - wait longer.
+  if (MyBuffer_num < 0){return;}
+  //buffer number changed? disconnect
+  if ((ringbuf[MyBuffer]->number != MyBuffer_num)){
+    disconnect();
+    return;
+  }
+  SWBaseSocket::SWBaseError err;
+  int ret = Conn->fsend(ringbuf[MyBuffer]->data, ringbuf[MyBuffer]->size, &err);
+  if ((err != SWBaseSocket::ok) && (err != SWBaseSocket::notReady)){
+    disconnect();
+    return;
+  }
+  if (ret == ringbuf[MyBuffer]->size){
+    //completed a send - switch to next buffer
+    MyBuffer++;
+    MyBuffer %= buffers;
+    MyBuffer_num = -1;
+  }
+}

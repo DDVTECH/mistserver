41901bb2aabac581942502324762159cffbc2829
Thulinma <thulinma@localhost.localdomain>
2010-08-11 00:07:23 +0200

    Betere server

--- Server/user.cpp
+++ Server/user.cpp
@@ -4,10 +4,9 @@
 
 class user{
   public:
-    user();
+    user(SWBaseSocket * newConn);
     ~user();
-    void disconnect();
-    void connect(SWBaseSocket * newConnection);
+    void disconnect(std::string reason);
     void Send(buffer ** ringbuf, int buffers);
     bool is_connected;
     SWUnixSocket * Conn;
@@ -16,28 +15,20 @@ class user{
 };//user
 
 
-user::user() {
-  Conn = NULL;
-  is_connected = false;
+user::user(SWBaseSocket * newConn) {
+  Conn = (SWUnixSocket*)newConn;
+  is_connected = (Conn != 0);
 }
 
-user::~user() {
-  Conn->disconnect();
-  Conn = NULL;
-  is_connected = false;
-}
+user::~user(){disconnect("Destroying object");}
 
-void user::disconnect() {
+void user::disconnect(std::string reason) {
   if (Conn) {
     Conn->disconnect();
     Conn = NULL;
+    std::cout << "Disconnected user: " << reason << std::endl;
   }
-  std::cout << "Disconnected user" << std::endl;
   is_connected = false;
-}
-void user::connect(SWBaseSocket * newConn) {
-  Conn = (SWUnixSocket*)newConn;
-  is_connected = (Conn != 0);
 }
 
 void user::Send(buffer ** ringbuf, int buffers){
@@ -49,13 +40,13 @@ void user::Send(buffer ** ringbuf, int buffers){
   if (MyBuffer_num < 0){return;}
   //buffer number changed? disconnect
   if ((ringbuf[MyBuffer]->number != MyBuffer_num)){
-    disconnect();
+    disconnect("Buffer number changed (connection too slow)");
     return;
   }
   SWBaseSocket::SWBaseError err;
   int ret = Conn->fsend(ringbuf[MyBuffer]->FLV->data, ringbuf[MyBuffer]->FLV->len, &err);
   if ((err != SWBaseSocket::ok) && (err != SWBaseSocket::notReady)){
-    disconnect();
+    disconnect("Socket error");
     return;
   }
   if (ret == ringbuf[MyBuffer]->FLV->len){

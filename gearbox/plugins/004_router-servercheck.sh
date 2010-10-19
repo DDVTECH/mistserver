752aa90d6a9e7e4dade0fc54f7c94cafe4874b71
Erik Zandvliet <s.p.the.ghost@gmail.com>
2010-10-19 13:27:27 +0200

    Router-servercheck module finished, contains also functions needed for the reconfiguration module

--- gearbox/plugins/004_router-servercheck.sh
+++ gearbox/plugins/004_router-servercheck.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-config_server1_alpha="INPUT=bla ROUTER_SOURCE=meh ROUTER_USEDBY=0"
+config_direct_alpha=""
+config_direct_beta=""
+config_direct_gamma=""
+config_direct_delta=""
+config_direct_epsilon=""
+config_direct_omega=""
+config_direct_theta=""
+config_direct_pi=""
+config_direct_meh=""
+config_direct_bla=""
+config_direct_koekjes=""
+
+config_server1_alpha=""
 config_server1_beta=""
 config_server1_gamma=""
 config_server1_delta=""
@@ -30,6 +42,8 @@ config_server9_meh=""
 config_server10_bla=""
 config_server10_koekjes=""
 
+server1_isup="0"
+
 update( ) {
   local target=$1
   local var=$2
@@ -37,6 +51,7 @@ update( ) {
   local string=${!target}
   local length=${#var}
   local loopstring=${string:i:length}
+  local i=0
   for ((i=0; i<${#string}; i++ )); do
     local loopstring=${string:i:length}
     if [ "$loopstring" == "$var" ]; then
@@ -49,28 +64,81 @@ update( ) {
     local varstring=${string:i}
     local limit=`expr index "$varstring" ' '`
     local newstring="${var}=${value}"
-    eval $target="\"${string/${varstring:0:(limit-1)}/$newstring}\""
+    if [ "$limit" != "0" ]; then
+      eval $target="\"${string/${varstring:0:(limit-1)}/$newstring}\""
+    else
+      eval $target="\"${string/${varstring}/$newstring}\""
+    fi
   fi
   local string=${!target}
-  echo $string
+}
+
+get_source( ) {
+  local conf="config_$1_$2"
+  local ${!conf}
+  echo $ROUTER_SOURCE
+}
+
+get_usedby( ) {
+  local ${!1}
+  echo $ROUTER_USEDBY
 }
 
 check_servers( ) {
   local i=0
   for ((i=0; i<${#servers[@]}; i++ )); do
-    echo "Checking server ${servers[i]}"
-    local isup="server_${servers[i]}_isup"
-    isup=${!isup}
-    if [[ $isup -eq 0 ]]; then
-      echo "  Server is up"
+    local isup="${servers[i]}_isup"
+    local isup=${!isup}
+    if [ "$isup" == "0" ]; then
+      local j=0
+      local streams="server_${servers[i]}"
+      local streams=( ${!streams} )
+      for ((j=0; j<${#streams[@]}; j++)); do
+        local sourceval=`get_source ${servers[i]} ${streams[j]}`
+        if [ "$sourceval" != "" ]; then
+          local target="config_${sourceval}_${streams[j]}"
+          local usage=`get_usedby $target`
+          local usage=$((${usage}-1))
+          update $target "ROUTER_USEDBY" $usage
+        fi
+        local target="config_${servers[i]}_${streams[j]}"
+        update $target "INPUT" ""
+        update $target "ROUTER_SOURCE" ""
+        update $target "ROUTER_USEDBY" "0"
+      done
     else
-      echo "  Server is down"
+      local j=0
+      local streams="server_${servers[i]}"
+      local streams=( ${!streams} )
+      for ((j=0; j<${#streams[@]}; j++)); do
+        local sourceval=`get_source ${servers[i]} ${streams[j]}`
+        if [ "$sourceval" != "" ]; then
+          local sourceup="${sourceval}_isup"
+          local sourceup=${!sourceup}
+          if [ "$sourceup" == "0" ]; then
+            local target="config_${servers[i]}_${streams[j]}"
+            update $target "ROUTER_SOURCE" ""
+            update $target "INPUT" ""
+          fi
+        fi
+      done
     fi
   done
 }
 
-#check_servers
-target="config_server1_alpha"
-var="ROUTER_SOURCE"
-val=""
-update $target $var $val
+print_conf( ) {
+  local i=0
+  for ((i=0; i<${#servers[@]}; i++ )); do
+    local streams="server_${servers[i]}"
+    local streams=( ${!streams} )
+    local j=0
+    for ((j=0; j<${#streams[@]}; j++ )); do
+      local target="config_${servers[i]}_${streams[j]}"
+      echo "${target}: ${!target}"
+    done
+  done
+}
+
+print_conf
+check_servers
+print_conf

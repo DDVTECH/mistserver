a8fc16159e5804950768ab50dda3ab37cfa62455
Thulinma <jaron@vietors.com>
2010-10-24 23:45:06 +0200

    Optimized mandriva prepare script

--- client_scripts/mandriva_prepare.sh
+++ client_scripts/mandriva_prepare.sh
@@ -7,11 +7,21 @@ HAS_SSL=`rpm -qa | grep openssl | grep devel`
 HAS_XIN=`rpm -qa | grep xinetd`
 HAS_GIT=`rpm -qa | grep git`
 
-if [ -z "$HAS_GCC" ]; then; DODOWN=1; fi
-if [ -z "$HAS_FFM" ]; then; DODOWN=1; fi
-if [ -z "$HAS_SSL" ]; then; DODOWN=1; fi
-if [ -z "$HAS_XIN" ]; then; DODOWN=1; fi
-if [ -z "$HAS_GIT" ]; then; DODOWN=1; fi
+if [ -z "$HAS_GCC" ]; then
+	DODOWN=1
+fi
+if [ -z "$HAS_FFM" ]; then
+	DODOWN=1
+fi
+if [ -z "$HAS_SSL" ]; then
+	DODOWN=1
+fi
+if [ -z "$HAS_XIN" ]; then
+	DODOWN=1
+fi
+if [ -z "$HAS_GIT" ]; then
+	DODOWN=1
+fi
 
 
 if [ "$DODOWN" -eq "0" ]; then

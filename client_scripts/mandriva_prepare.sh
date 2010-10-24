e97873dc2712213aa31d13c6a762de68ce031bae
Thulinma <jaron@vietors.com>
2010-10-24 22:41:36 +0200

    Mandriva PLF media en ffmpeg support

--- client_scripts/mandriva_prepare.sh
+++ client_scripts/mandriva_prepare.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+urpmi.removemedia -a
+urpmi.addmedia --all-media --distrib --mirrorlist '$MIRRORLIST'
+urpmi.addmedia --all-media --distrib --mirrorlist 'http://plf.zarb.org/mirrors/$RELEASE.$ARCH.list'
 urpmi.update -a
-urpmi --auto --force git openssl-devel xinetd
+urpmi --auto --force git openssl-devel xinetd ffmpeg
 
 

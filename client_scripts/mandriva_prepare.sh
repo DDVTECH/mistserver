2f429ab01a46eb7f8045e942ed68e5bcdfa0586f
Thulinma <jaron@vietors.com>
2010-10-24 23:27:56 +0200

    Fixed install voor live 2

--- client_scripts/mandriva_prepare.sh
+++ client_scripts/mandriva_prepare.sh
@@ -4,6 +4,6 @@ urpmi.removemedia -a
 urpmi.addmedia --all-media --distrib --mirrorlist '$MIRRORLIST'
 urpmi.addmedia --all-media --distrib --mirrorlist 'http://plf.zarb.org/mirrors/$RELEASE.$ARCH.list'
 urpmi.update -a
-urpmi --auto --force git openssl-devel xinetd ffmpeg make gcc-g++
+urpmi --auto --force git openssl-devel xinetd ffmpeg make gcc-c++
 
 

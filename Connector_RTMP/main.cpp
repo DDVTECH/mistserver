2529d5c0f016fb88a5c438e4b05ecb49de36beb9
Thulinma <jaron@vietors.com>
2010-11-08 15:28:56 +0100

    Nieuwe flv parser, nieuwe unix sockets support...

--- Connector_RTMP/main.cpp
+++ Connector_RTMP/main.cpp
@@ -10,15 +10,14 @@
 #include <sys/epoll.h>
 
 //for connection to server
-#include "../sockets/SocketW.h"
 bool ready4data = false;//set to true when streaming starts
 bool inited = false;
 bool stopparsing = false;
 timeval lastrec;
 
 int CONN_fd = 0;
-#include "../util/flv_sock.cpp" //FLV parsing with SocketW
 #include "../util/ddv_socket.cpp" //DDVTech Socket wrapper
+#include "../util/flv_sock.cpp" //FLV parsing with SocketW
 #include "parsechunks.cpp" //chunkstream parsing
 #include "handshake.cpp" //handshaking
 
@@ -64,7 +63,8 @@ int main(){
   unsigned int ts;
   unsigned int fts = 0;
   unsigned int ftst;
-  SWUnixSocket ss;
+  int ss;
+  FLV_Pack * tag;
 
   //first timestamp set
   firsttime = getNowMS();
@@ -98,7 +98,7 @@ int main(){
   
     
   
-  while (!socketError){
+  while (!socketError && !All_Hell_Broke_Loose){
     //only parse input if available or not yet init'ed
     //rightnow = getNowMS();
     retval = epoll_wait(poller, events, 1, 0);
@@ -108,51 +108,44 @@ int main(){
     if (ready4data){
       if (!inited){
         //we are ready, connect the socket!
-        if (!ss.connect(streamname.c_str())){
+        ss = DDV_OpenUnix(streamname.c_str());
+        if (ss <= 0){
           #ifdef DEBUG
           fprintf(stderr, "Could not connect to server!\n");
           #endif
           return 0;
         }
-        FLV_Readheader(ss);//read the header, we don't want it
         #ifdef DEBUG
-        fprintf(stderr, "Header read, starting to send video data...\n");
+        fprintf(stderr, "Everything connected, starting to send video data...\n");
         #endif
         inited = true;
       }
       //only send data if previous data has been ACK'ed...
       if (snd_cnt - snd_window_at < snd_window_size){
-        if (FLV_GetPacket(ss)){//able to read a full packet?
-          ts = FLVbuffer[7] * 256*256*256;
-          ts += FLVbuffer[4] * 256*256;
-          ts += FLVbuffer[5] * 256;
-          ts += FLVbuffer[6];
+        if (FLV_GetPacket(tag, ss)){//able to read a full packet?
+          ts = tag->data[7] * 256*256*256;
+          ts += tag->data[4] * 256*256;
+          ts += tag->data[5] * 256;
+          ts += tag->data[6];
           if (ts != 0){
             if (fts == 0){fts = ts;ftst = getNowMS();}
             ts -= fts;
-            FLVbuffer[7] = ts / (256*256*256);
-            FLVbuffer[4] = ts / (256*256);
-            FLVbuffer[5] = ts / 256;
-            FLVbuffer[6] = ts % 256;
+            tag->data[7] = ts / (256*256*256);
+            tag->data[4] = ts / (256*256);
+            tag->data[5] = ts / 256;
+            tag->data[6] = ts % 256;
             ts += ftst;
           }else{
             ftst = getNowMS();
-            FLVbuffer[7] = ftst / (256*256*256);
-            FLVbuffer[4] = ftst / (256*256);
-            FLVbuffer[5] = ftst / 256;
-            FLVbuffer[6] = ftst % 256;
+            tag->data[7] = ftst / (256*256*256);
+            tag->data[4] = ftst / (256*256);
+            tag->data[5] = ftst / 256;
+            tag->data[6] = ftst % 256;
           }
-          SendMedia((unsigned char)FLVbuffer[0], (unsigned char *)FLVbuffer+11, FLV_len-15, ts);
+          SendMedia((unsigned char)tag->data[0], (unsigned char *)tag->data+11, tag->len-15, ts);
           #ifdef DEBUG
           fprintf(stderr, "Sent a tag to %i\n", CONN_fd);
           #endif
-          FLV_Dump();//dump packet and get ready for next
-        }
-        if ((SWBerr != SWBaseSocket::ok) && (SWBerr != SWBaseSocket::notReady)){
-          #ifdef DEBUG
-          fprintf(stderr, "No more data! :-(  (%s)\n", SWBerr.get_error().c_str());
-          #endif
-          return 0;//no more input possible! Fail immediately.
         }
       }
     }
--- util/ddv_socket.cpp
+++ util/ddv_socket.cpp
@@ -1,12 +1,33 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <fcntl.h>
 
 bool socketError = false;
+
+int DDV_OpenUnix(const char adres[], bool nonblock = false){
+  int s = socket(AF_UNIX, SOCK_STREAM, 0);
+  sockaddr_un addr;
+  addr.sun_family = AF_UNIX;
+  strcpy(addr.sun_path, adres);
+  int r = connect(s, (sockaddr*)&adres, sizeof(addr));
+  if (r == 0){
+    if (nonblock){
+      int flags = fcntl(s, F_GETFL, 0);
+      flags |= O_NONBLOCK;
+      fcntl(s, F_SETFL, flags);
+    }
+    return s;
+  }else{
+    close(s);
+    return 0;
+  }
+}
 
 int DDV_Listen(int port){
   int s = socket(AF_INET, SOCK_STREAM, 0);
@@ -36,9 +57,8 @@ int DDV_Accept(int sock){
   return accept(sock, 0, 0);
 }
 
-bool DDV_write(void * buffer, int width, int count, int sock){
+bool DDV_write(void * buffer, int todo, int sock){
   int sofar = 0;
-  int todo = width*count;
   while (sofar != todo){
     int r = send(sock, (char*)buffer + sofar, todo-sofar, 0);
     if (r <= 0){
@@ -51,9 +71,8 @@ bool DDV_write(void * buffer, int width, int count, int sock){
   return true;
 }
 
-bool DDV_read(void * buffer, int width, int count, int sock){
+bool DDV_read(void * buffer, int todo, int sock){
   int sofar = 0;
-  int todo = width*count;
   while (sofar != todo){
     int r = recv(sock, (char*)buffer + sofar, todo-sofar, 0);
     if (r <= 0){
@@ -65,3 +84,28 @@ bool DDV_read(void * buffer, int width, int count, int sock){
   }
   return true;
 }
+
+
+bool DDV_read(void * buffer, int width, int count, int sock){return DDV_read(buffer, width*count, sock);}
+bool DDV_write(void * buffer, int width, int count, int sock){return DDV_write(buffer, width*count, sock);}
+
+
+int DDV_iwrite(void * buffer, int todo, int sock){
+  int r = send(sock, buffer, todo, 0);
+  if (r < 0){
+    socketError = true;
+    printf("Could not write! %s\n", strerror(errno));
+  }
+  return r;
+}
+
+int DDV_iread(void * buffer, int todo, int sock){
+  int r = recv(sock, buffer, todo, 0);
+  if (r < 0){
+    socketError = true;
+    printf("Could not write! %s\n", strerror(errno));
+  }
+  return r;
+}
+
+

fa5408457a343af8a08dd54e40a4543953b5be6e
Thulinma <jaron@vietors.com>
2010-11-09 03:33:10 +0100

    Timestamp fix

--- gearbox/write_configs.sh
+++ gearbox/write_configs.sh
@@ -27,12 +27,13 @@ function UploadConfig (){
 
 echo_green "Uploading server configurations..."
 count=${#servers[@]}
+TIMECODE=`date +%s`
 for ((j=0; j < count; j++)); do
   eval "isup=\$${servers[$j]}_isup"
   UploadConfig ${servers[$j]}
   if [ "$isup" -eq "1" ]; then
-    wget -qO /dev/null "http://ddvtech.com/gearbox_report.php?serveron=${servers[$j]}"
+    wget -qO /dev/null "http://ddvtech.com/gearbox_report.php?serveron=${servers[$j]}\&time=$TIMECODE"
   else
-    wget -qO /dev/null "http://ddvtech.com/gearbox_report.php?serveroff=${servers[$j]}"
+    wget -qO /dev/null "http://ddvtech.com/gearbox_report.php?serveroff=${servers[$j]}\&time=$TIMECODE"
   fi
 done

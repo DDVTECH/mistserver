bd0664928aea9c59e7fc8768273fefd5ca0834fe
Erik Zandvliet <s.p.the.ghost@gmail.com>
2011-02-07 10:55:51 +0100

    ABST structure updated, contains al private data now

--- MP4/box_abst.h
+++ MP4/box_abst.h
@@ -1,14 +1,34 @@
 #include "box.h"
+#include <string>
+#include <vector>
+
+struct abst_serverentry {
+  std::string ServerBaseUrl;
+};//abst_serverentry
+
+struct abst_qualityentry {
+  std::string QualityModifier;
+};//abst_qualityentry
 
 class Box_abst {
   public:
     Box_abst( );
     ~Box_abst();
     Box * GetBox();
-    
+
   private:
     uint8_t curProfile;
     bool isLive;
     bool isUpdate;
+    uint32_t curTimeScale;
+    uint32_t curMediatime;//write as uint64_t
+    uint32_t curSMPTE;//write as uint64_t
+    std::string curMovieIdentifier;
+    std::string curDRM;
+    std::string curMetaData;
+    std::vector<abst_serverentry> Servers;
+    std::vector<abst_qualityentry> Qualities;
+    std::vector<Box *> SegmentRunTables;
+    std::vector<Box *> FragmentRunTables;
     Box * Container;
 };//Box_ftyp Class

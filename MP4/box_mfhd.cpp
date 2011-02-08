1e4645c962fab3bb0fcf8bd8e87d5116a277e331
Erik Zandvliet <s.p.the.ghost@gmail.com>
2011-02-08 15:31:00 +0100

    MFHD box

---
+++ MP4/box_mfhd.cpp
@@ -0,0 +1,27 @@
+#include "box_mfhd.h"
+
+Box_mfhd::Box_mfhd( ) {
+  Container = new Box( 0x6D666864 );
+  SetDefaults( );
+  SetReserved( )
+}
+
+Box_mfhd::~Box_mfhd() {
+  delete Container;
+}
+
+Box * Box_mfhd::GetBox() {
+  return Container;
+}
+
+void Box_mfhd::SetDefaults( ) {
+  SetSequenceNumber( );
+}
+
+void Box_mfhd::SetDefaults( ) {
+  Container->SetPayload((uint32_t)4,Box::uint32_to_uint8(0));
+}
+
+void SetSequenceNumber( uint32_t SequenceNumber );
+  Container->SetPayload((uint32_t)4,Box::uint32_to_uint8(SequenceNumber),4);
+}

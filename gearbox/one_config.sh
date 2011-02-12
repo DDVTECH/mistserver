bb7e70bbafb568f3d8221f84c48ab32021af7083
Thulinma <jaron@vietors.com>
2011-02-12 22:38:14 +0100

    En nu ook met werkende getallen en zo. Erg cool allemaal.

--- gearbox/one_config.sh
+++ gearbox/one_config.sh
@@ -6,8 +6,8 @@ if [ $? -ne 0 ]; then
   echo $1_isup=0 > ./server_info/config_$1.sh
   echo isup=0 >> ./server_info/config_$1.sh
 else
-  eval tmpvals=`ssh -o PasswordAuthentication=no -o ConnectTimeout=6 -p $PORT root@$HOST "./data_collect.sh 2> /dev/null"`
+  eval tmpvals='`ssh -o PasswordAuthentication=no -o ConnectTimeout=6 -p $PORT root@$HOST "./data_collect.sh 2> /dev/null"`'
   echo $1_isup=1 > ./server_info/config_$1.sh
   echo isup=1 >> ./server_info/config_$1.sh
-  echo $1_status="$tmpvals" >> ./server_info/config_$1.sh
+  echo "$1_status=""$tmpvals""" >> ./server_info/config_$1.sh
 fi

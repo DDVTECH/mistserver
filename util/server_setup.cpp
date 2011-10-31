2468bd288b87e6a482d81a2e1423988749247bca
Thulinma <jaron@vietors.com>
2011-10-31 15:23:46 +0100

    Better automatic version numbering (now for all processes), added -v switch for version display to all processes.

--- util/server_setup.cpp
+++ util/server_setup.cpp
@@ -22,12 +22,11 @@
 #endif
 
 #include "socket.h" //Socket library
+#include "util.h" //utilities for process spawning and config management
 #include <signal.h>
 #include <sys/types.h>
 #include <pwd.h>
 #include <fstream>
-#define defstr(x) #x ///< converts a define name to string
-#define defstrh(x) "[" defstr(x) "]" ///< converts define name to [string]
 Socket::Server server_socket(-1); ///< Placeholder for the server socket
 
 /// Basic signal handler. Disconnects the server_socket if it receives
@@ -81,115 +80,28 @@ int main(int argc, char ** argv){
   sigaction(SIGTERM, &new_action, NULL);
   sigaction(SIGPIPE, &new_action, NULL);
   sigaction(SIGCHLD, &new_action, NULL);
-  
-  //default values
-  int listen_port = DEFAULT_PORT;
-  bool daemon_mode = true;
-  std::string interface = "0.0.0.0";
-  std::string configfile = "/etc/ddvtech.conf";
-  std::string username = "root";
-  bool ignore_daemon = false;
-  bool ignore_interface = false;
-  bool ignore_port = false;
-  bool ignore_user = false;
-  
-  int opt = 0;
-  static const char *optString = "ndp:i:u:c:h?";
-  static const struct option longOpts[] = {
-    {"help",0,0,'h'},
-    {"port",1,0,'p'},
-    {"interface",1,0,'i'},
-    {"username",1,0,'u'},
-    {"no-daemon",0,0,'n'},
-    {"daemon",0,0,'d'},
-    {"configfile",1,0,'c'}
-  };
-  while ((opt = getopt_long(argc, argv, optString, longOpts, 0)) != -1){
-    switch (opt){
-      case 'p': listen_port = atoi(optarg); ignore_port = true; break;
-      case 'i': interface = optarg; ignore_interface = true; break;
-      case 'n': daemon_mode = false; ignore_daemon = true; break;
-      case 'd': daemon_mode = true; ignore_daemon = true; break;
-      case 'c': configfile = optarg; break;
-      case 'u': username = optarg; ignore_user = true; break;
-      case 'h':
-      case '?':
-        printf("Options: -h[elp], -?, -n[odaemon], -d[aemon], -p[ort] VAL, -i[nterface] VAL, -c[onfigfile] VAL, -u[sername] VAL\n");
-        printf("Defaults:\n  interface: 0.0.0.0\n  port: %i\n  daemon mode: true\n  configfile: /etc/ddvtech.conf\n  username: root\n", listen_port);
-        printf("Username root means no change to UID, no matter what the UID is.\n");
-        printf("If the configfile exists, it is always loaded first. Commandline settings then overwrite the config file.\n");
-        printf("\nThis process takes it directives from the %s section of the configfile.\n", defstrh(CONFIGSECT));
-        return 1;
-        break;
-    }
-  }//commandline options parser
 
-  std::ifstream conf(configfile.c_str(), std::ifstream::in);
-  std::string tmpstr;
-  bool acc_comm = false;
-  size_t foundeq;
-  if (conf.fail()){
-    #if DEBUG >= 3
-    fprintf(stderr, "Configuration file %s not found - using build-in defaults...\n", configfile.c_str());
-    #endif
-  }else{
-    while (conf.good()){
-      getline(conf, tmpstr);
-      if (tmpstr[0] == '['){//new section? check if we care.
-        if (tmpstr == defstrh(CONFIGSECT)){acc_comm = true;}else{acc_comm = false;}
-      }else{
-        if (!acc_comm){break;}//skip all lines in this section if we do not care about it
-        foundeq = tmpstr.find('=');
-        if (foundeq != std::string::npos){
-          if ((tmpstr.substr(0, foundeq) == "port") && !ignore_port){listen_port = atoi(tmpstr.substr(foundeq+1).c_str());}
-          if ((tmpstr.substr(0, foundeq) == "interface") && !ignore_interface){interface = tmpstr.substr(foundeq+1);}
-          if ((tmpstr.substr(0, foundeq) == "username") && !ignore_user){username = tmpstr.substr(foundeq+1);}
-          if ((tmpstr.substr(0, foundeq) == "daemon") && !ignore_daemon){daemon_mode = true;}
-          if ((tmpstr.substr(0, foundeq) == "nodaemon") && !ignore_daemon){daemon_mode = false;}
-        }//found equals sign
-      }//section contents
-    }//configfile line loop
-  }//configuration
+  //set and parse configuration
+  Util::Config C;
+  C.confsection = TOSTRING(CONFIGSECT);
+  C.listen_port = DEFAULT_PORT;
+  C.parseArgs(argc, argv);
+  C.parseFile();
 
   //setup a new server socket, for the correct interface and port
-  server_socket = Socket::Server(listen_port, interface);
+  server_socket = Socket::Server(C.listen_port, C.interface);
   #if DEBUG >= 3
-  fprintf(stderr, "Made a listening socket on %s:%i...\n", interface.c_str(), listen_port);
+  fprintf(stderr, "Made a listening socket on %s:%i...\n", C.interface.c_str(), C.listen_port);
   #endif
-  if (server_socket.connected()){
-    //if setup success, enter daemon mode if requested
-    if (daemon_mode){
-      #if DEBUG >= 3
-      fprintf(stderr, "Going into background mode...\n");
-      #endif
-      daemon(1, 0);
-    }
-  }else{
+  if (!server_socket.connected()){
     #if DEBUG >= 1
     fprintf(stderr, "Error: could not make listening socket\n");
     #endif
     return 1;
   }
 
-  if (username != "root"){
-    struct passwd * user_info = getpwnam(username.c_str());
-    if (!user_info){
-      #if DEBUG >= 1
-      fprintf(stderr, "Error: could not setuid %s: could not get PID\n", username.c_str());
-      #endif
-      return 1;
-    }else{
-      if (setuid(user_info->pw_uid) != 0){
-        #if DEBUG >= 1
-        fprintf(stderr, "Error: could not setuid %s: not allowed\n", username.c_str());
-        #endif
-      }else{
-        #if DEBUG >= 3
-        fprintf(stderr, "Changed user to %s\n", username.c_str());
-        #endif
-      }
-    }
-  }
+  Util::setUser(C.username);
+  if (C.daemon_mode){Util::Daemonize();}
 
   while (server_socket.connected()){
     S = server_socket.accept();

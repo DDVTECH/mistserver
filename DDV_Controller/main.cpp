eff3fd9987251209c5781a09d8edbff1e8dbf2a6
Thulinma <jaron@vietors.com>
2011-12-04 21:02:51 +0100

    Fixed another stats bug

--- DDV_Controller/main.cpp
+++ DDV_Controller/main.cpp
@@ -450,7 +450,7 @@ int main(int argc, char ** argv){
         if (it->Received() != ""){
           size_t newlines = it->Received().find("\n\n");
           while (newlines != std::string::npos){
-            if (!JsonParse.parse(it->Received().substr(0, newlines), Request, false)){
+            if (JsonParse.parse(it->Received().substr(0, newlines), Request, false)){
               Log("STAT", "Received stats from a buffer: "+Request.toStyledString());
               if (Request.isMember("totals") && Request["totals"].isMember("buffer")){
                 std::string thisbuffer = Request["totals"]["buffer"].asString();

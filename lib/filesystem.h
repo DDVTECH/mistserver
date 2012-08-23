0ac64dd6cb490ca62374eda9163e4f6e4a71d1ba
Erik Zandvliet <erik.zandvliet@ddvtech.com>
2012-08-23 12:06:26 +0200

    FTP Convert to LibMist

---
+++ lib/filesystem.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <sstream>
+#include <fstream>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <locale.h>
+#include <langinfo.h>
+#include <stdint.h>
+#include <errno.h>
+
+namespace Filesystem {
+  enum DIR_Permissions {
+    P_LIST = 0x01,//List
+    P_RETR = 0x02,//Retrieve
+    P_STOR = 0x04,//Store
+    P_RNFT = 0x08,//Rename From/To
+    P_DELE = 0x10,//Delete
+    P_MKD = 0x20,//Make directory
+    P_RMD = 0x40,//Remove directory
+  };
+  
+  enum DIR_Show {
+    S_NONE = 0x00,
+    S_ACTIVE = 0x01,
+    S_INACTIVE = 0x02,
+    S_ALL = 0x03,
+  };
+  
+  class Directory {
+    public:
+      Directory( std::string PathName = "", std::string BasePath = ".");
+      ~Directory( );
+      void Print( );
+      bool IsDir( );
+      std::string PWD( );
+      std::string LIST( std::vector<std::string> ActiveStreams = std::vector<std::string>() );
+      bool CWD( std::string Path );
+      bool CDUP( );
+      bool DELE( std::string Path );
+      bool MKD( std::string Path );
+      std::string RETR( std::string Path );
+      void STOR( std::string Path, std::string Data );
+      bool Rename( std::string From, std::string To );
+      void SetPermissions( std::string PathName, char Permissions );
+      bool HasPermission( char Permission );
+      void SetVisibility( std::string Pathname, char Visible );
+    private:
+      bool ValidDir;
+      bool SimplifyPath( );
+      void FillEntries( );
+      std::string MyBase;
+      std::string MyPath;
+      std::map< std::string, struct stat > Entries;
+      std::map< std::string, char > MyPermissions;
+      std::map< std::string, char > MyVisible;
+  };//Directory Class
+};//Filesystem namespace
---
+++ lib/ftp.h
@@ -0,0 +1,77 @@
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "./socket.h"
+#include "./filesystem.h"
+#include <unistd.h>
+
+#include "./json.h"
+
+namespace FTP {
+  static std::string FTPBasePath = "/tmp/mist/OnDemand/";
+  
+  enum Mode {
+    MODE_STREAM,
+  };//FTP::Mode enumeration
+  
+  enum Structure {
+    STRU_FILE,
+    STRU_RECORD,
+  };//FTP::Structure enumeration
+  
+  enum Type {
+    TYPE_ASCII_NONPRINT,
+    TYPE_IMAGE_NONPRINT,
+  };//FTP::Type enumeration
+  
+  enum Commands {
+    CMD_NOCMD,
+    CMD_NOOP,
+    CMD_USER,
+    CMD_PASS,
+    CMD_QUIT,
+    CMD_PORT,
+    CMD_RETR,
+    CMD_STOR,
+    CMD_TYPE,
+    CMD_MODE,
+    CMD_STRU,
+    CMD_EPSV,
+    CMD_PASV,
+    CMD_LIST,
+    CMD_PWD,
+    CMD_CWD,
+    CMD_CDUP,
+    CMD_DELE,
+    CMD_RMD,
+    CMD_MKD,
+    CMD_RNFR,
+    CMD_RNTO,
+  };//FTP::Commands enumeration
+  
+  class User {
+    public:
+      User( Socket::Connection NewConnection = Socket::Connection() );
+      ~User( );
+      int ParseCommand( std::string Command );
+      bool LoggedIn( );
+      std::string NumToMsg( int MsgNum );
+      Socket::Connection Conn;
+    private:
+      std::string USER;
+      std::string PASS;
+      Mode MODE;
+      Structure STRU;
+      Type TYPE;
+      int PORT;
+      Socket::Server Passive;
+      int MyPassivePort;
+      Filesystem::Directory MyDir;
+      std::string RNFR;
+      std::vector< std::string > ActiveStreams;
+  };//FTP::User class
+  
+};//FTP Namespace

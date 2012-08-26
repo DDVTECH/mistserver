516c8bf091893246398358d893475fa5038fd729
Thulinma <jaron@vietors.com>
2012-08-26 21:18:40 +0200

    Fixed hook creator to also work when pulling.

--- createhooks.sh
+++ createhooks.sh
@@ -2,7 +2,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo -e "#!/bin/bash\n[ -f configure ] && touch configure\n[ -f configure.ac ] && touch configure.ac" > $DIR/.git/hooks/post-commit
 echo -e "#!/bin/bash\n[ -f configure ] && touch configure\n[ -f configure.ac ] && touch configure.ac" > $DIR/.git/hooks/post-checkout
+echo -e "#!/bin/bash\n[ -f configure ] && touch configure\n[ -f configure.ac ] && touch configure.ac" > $DIR/.git/hooks/post-receive
 chmod +x $DIR/.git/hooks/post-commit
 chmod +x $DIR/.git/hooks/post-checkout
+chmod +x $DIR/.git/hooks/post-receive
 echo "Done! The version number should now auto-update whenever you commit or checkout."
 

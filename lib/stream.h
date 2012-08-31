3ef44546a878eb02f4855a46812319fa1da47714
Thulinma <jaron@vietors.com>
2012-08-31 16:55:28 +0200

    Utils::Stream::getStream now actually works in all cases where it should, the way it should. ^_^

--- lib/stream.h
+++ lib/stream.h
@@ -7,17 +7,11 @@
 
 namespace Util{
   class Stream{
-    /// Sanitize a streamname.
-    static void sanitizeName(std::string & streamname);
-  public:
-    /// Get a connection to a Live stream.
-    static Socket::Connection getLive(std::string streamname);
-    /// Get a connection to a VoD stream.
-    static Socket::Connection getVod(std::string streamname);
-    /// Probe for available streams. Currently first VoD, then Live.
-    static Socket::Connection getStream(std::string streamname);
-
-    /// Create a Live stream on the system.
-    static Socket::Server makeLive(std::string streamname);
+    public:
+      static void sanitizeName(std::string & streamname);
+      static Socket::Connection getLive(std::string streamname);
+      static Socket::Connection getVod(std::string streamname);
+      static Socket::Connection getStream(std::string streamname);
+      static Socket::Server makeLive(std::string streamname);
   };
 }

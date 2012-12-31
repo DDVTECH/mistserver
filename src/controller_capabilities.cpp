49849e9d59f21c6cae0a7ea804b63832e8c7a215
Thulinma <jaron@vietors.com>
2012-12-31 16:39:05 +0100

    Added TS support to controller, updated TS-related coding style for merge.

--- src/controller_capabilities.cpp
+++ src/controller_capabilities.cpp
@@ -159,9 +159,25 @@ namespace Controller {
     capa["connectors"]["RTMP"]["optional"]["interface"]["help"] = "Address of the interface to listen on - default if unprovided is all interfaces";
     capa["connectors"]["RTMP"]["optional"]["interface"]["type"] = "str";
     capa["connectors"]["RTMP"]["optional"]["username"]["name"] = "Username";
-    capa["connectors"]["RTMP"]["optional"]["username"]["help"] = "Username to drop privileges to - default if unprovided means do not drop privileges";
+    capa["connectors"]["RTMP"]["optional"]["username"]["help"] =
+        "Username to drop privileges to - default if unprovided means do not drop privileges";
     capa["connectors"]["RTMP"]["optional"]["username"]["type"] = "str";
-    capa["connectors"]["HTTP"]["desc"] = "Enables the generic HTTP listener, required by all other HTTP protocols. Needs other HTTP protocols enabled to do much of anything.";
+    capa["connectors"]["TS"]["desc"] = "Enables the raw MPEG Transport Stream protocol over TCP.";
+    capa["connectors"]["TS"]["deps"] = "";
+    capa["connectors"]["TS"]["required"]["port"]["name"] = "TCP port";
+    capa["connectors"]["TS"]["required"]["port"]["help"] = "TCP port to listen on";
+    capa["connectors"]["TS"]["required"]["port"]["type"] = "uint";
+    capa["connectors"]["TS"]["required"]["args"]["name"] = "Stream";
+    capa["connectors"]["TS"]["required"]["args"]["help"] = "What streamname to serve - for multiple streams, add this protocol multiple times.";
+    capa["connectors"]["TS"]["required"]["args"]["type"] = "str";
+    capa["connectors"]["TS"]["optional"]["interface"]["name"] = "Interface";
+    capa["connectors"]["TS"]["optional"]["interface"]["help"] = "Address of the interface to listen on - default if unprovided is all interfaces";
+    capa["connectors"]["TS"]["optional"]["interface"]["type"] = "str";
+    capa["connectors"]["TS"]["optional"]["username"]["name"] = "Username";
+    capa["connectors"]["TS"]["optional"]["username"]["help"] = "Username to drop privileges to - default if unprovided means do not drop privileges";
+    capa["connectors"]["TS"]["optional"]["username"]["type"] = "str";
+    capa["connectors"]["HTTP"]["desc"] =
+        "Enables the generic HTTP listener, required by all other HTTP protocols. Needs other HTTP protocols enabled to do much of anything.";
     capa["connectors"]["HTTP"]["deps"] = "";
     capa["connectors"]["HTTP"]["optional"]["port"]["name"] = "TCP port";
     capa["connectors"]["HTTP"]["optional"]["port"]["help"] = "TCP port to listen on - default if unprovided is 8080";
@@ -170,22 +186,26 @@ namespace Controller {
     capa["connectors"]["HTTP"]["optional"]["interface"]["help"] = "Address of the interface to listen on - default if unprovided is all interfaces";
     capa["connectors"]["HTTP"]["optional"]["interface"]["type"] = "str";
     capa["connectors"]["HTTP"]["optional"]["username"]["name"] = "Username";
-    capa["connectors"]["HTTP"]["optional"]["username"]["help"] = "Username to drop privileges to - default if unprovided means do not drop privileges";
+    capa["connectors"]["HTTP"]["optional"]["username"]["help"] =
+        "Username to drop privileges to - default if unprovided means do not drop privileges";
     capa["connectors"]["HTTP"]["optional"]["username"]["type"] = "str";
     capa["connectors"]["HTTPProgressive"]["desc"] = "Enables HTTP protocol progressive streaming.";
     capa["connectors"]["HTTPProgressive"]["deps"] = "HTTP";
     capa["connectors"]["HTTPProgressive"]["optional"]["username"]["name"] = "Username";
-    capa["connectors"]["HTTPProgressive"]["optional"]["username"]["help"] = "Username to drop privileges to - default if unprovided means do not drop privileges";
+    capa["connectors"]["HTTPProgressive"]["optional"]["username"]["help"] =
+        "Username to drop privileges to - default if unprovided means do not drop privileges";
     capa["connectors"]["HTTPProgressive"]["optional"]["username"]["type"] = "str";
     capa["connectors"]["HTTPDynamic"]["desc"] = "Enables HTTP protocol Adobe-specific dynamic streaming (aka HDS).";
     capa["connectors"]["HTTPDynamic"]["deps"] = "HTTP";
     capa["connectors"]["HTTPDynamic"]["optional"]["username"]["name"] = "Username";
-    capa["connectors"]["HTTPDynamic"]["optional"]["username"]["help"] = "Username to drop privileges to - default if unprovided means do not drop privileges";
+    capa["connectors"]["HTTPDynamic"]["optional"]["username"]["help"] =
+        "Username to drop privileges to - default if unprovided means do not drop privileges";
     capa["connectors"]["HTTPDynamic"]["optional"]["username"]["type"] = "str";
     capa["connectors"]["HTTPSmooth"]["desc"] = "Enables HTTP protocol MicroSoft-specific smooth streaming through silverlight.";
     capa["connectors"]["HTTPSmooth"]["deps"] = "HTTP";
     capa["connectors"]["HTTPSmooth"]["optional"]["username"]["name"] = "Username";
-    capa["connectors"]["HTTPSmooth"]["optional"]["username"]["help"] = "Username to drop privileges to - default if unprovided means do not drop privileges";
+    capa["connectors"]["HTTPSmooth"]["optional"]["username"]["help"] =
+        "Username to drop privileges to - default if unprovided means do not drop privileges";
     capa["connectors"]["HTTPSmooth"]["optional"]["username"]["type"] = "str";
   }
 

9721efda0c75b68fce98cdc8c705d6663a661754
Thulinma <jaron@vietors.com>
2013-03-07 13:40:42 +0100

    Removed unneccesary temporary debugging code from FLV analyser.

--- src/analysers/flv_analyser.cpp
+++ src/analysers/flv_analyser.cpp
@@ -19,20 +19,10 @@ int main(int argc, char ** argv){
   Util::Config conf = Util::Config(argv[0], PACKAGE_VERSION);
   conf.parseArgs(argc, argv);
   FLV::Tag FLV_in; // Temporary storage for incoming FLV data.
-  std::ofstream vData("vData");
-  std::ofstream aData("aData");
   while ( !feof(stdin)){
     if (FLV_in.FileLoader(stdin)){
       std::cout << "Tag: " << FLV_in.tagType() << "\n\tTime: " << FLV_in.tagTime() << std::endl;
-      if (FLV_in.data[0] == 0x08){ //Audio
-        aData.write(FLV_in.data + 13, FLV_in.len - 17);
-      }
-      if (FLV_in.data[0] == 0x09){ //Video
-        vData.write(FLV_in.data + 16, FLV_in.len - 20);
-      }
     }
   }
-  vData.close();
-  aData.close();
   return 0;
 }

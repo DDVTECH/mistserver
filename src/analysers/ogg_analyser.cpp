f9266aeb767dc33e68a7ac9837b2a307a2efe383
Oswald de Bruin <oswald@mint>
2013-06-06 12:05:34 +0200

    ogg support started in lib

---
+++ src/analysers/ogg_analyser.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <mist/ogg.h>
+#include <mist/config.h>
+
+namespace Analysers{
+  int analyseOGG(){
+    std::string oggBuffer;
+    //Read all of std::cin to mp4Buffer
+    while (std::cin.good()){
+      oggBuffer += std::cin.get();
+    }
+    oggBuffer.erase(oggBuffer.size() - 1, 1);
+
+    OGG::Page oggData;
+    while (oggData.read(oggBuffer)){
+      std::cerr << oggData.toPrettyString() << std::endl;
+    }
+    return 0;
+  }
+}
+
+int main(int argc, char ** argv){
+  Util::Config conf = Util::Config(argv[0], PACKAGE_VERSION);
+  conf.parseArgs(argc, argv);
+  return Analysers::analyseOGG();
+}
+
---
+++ src/converters/ogg2dtsc.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <mist/dtsc.h>
+#include <mist/ogg.h>
+#include <mist/config.h>
+
+namespace Converters{
+  int OGG2DTSC(){
+    
+  }
+}
+
+int main(int argc, char ** argv){
+  
+}

4364199f503c0115d7cfdbfbb06cc437435cfc17
Erik Zandvliet <erik.zandvliet@ddvtech.com>
2013-06-24 09:37:16 +0200

    Updated ogg analyser

--- src/analysers/ogg_analyser.cpp
+++ src/analysers/ogg_analyser.cpp
@@ -1,13 +1,17 @@
 #include <cstdlib>
+#include <cstdio>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 #include <string.h>
 #include <mist/ogg.h>
 #include <mist/config.h>
+#include <mist/theora.h>
 
 namespace Analysers{
   int analyseOGG(){
+    std::map<int,std::string> sn2Codec;
     std::string oggBuffer;
     OGG::Page oggPage;
     //Read all of std::cin to oggBuffer
@@ -18,6 +22,13 @@ namespace Analysers{
       }
       //while OGG::page check function read
       while (oggPage.read(oggBuffer)){//reading ogg to string
+        oggPage.setInternalCodec("");
+        if (oggPage.getHeaderType() & 0x02){
+          if (memcmp("theora",oggPage.getFullPayload() + 1,6) == 0){
+            sn2Codec[oggPage.getBitstreamSerialNumber()] = "theora";
+          }
+        }
+        oggPage.setInternalCodec(sn2Codec[oggPage.getBitstreamSerialNumber()]);
         std::cout << oggPage.toPrettyString() << std::endl;
       }
     }

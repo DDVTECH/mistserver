840cfaabed6d8ac57baec741639c6b1fe9760d1a
Thulinma <jaron@vietors.com>
2014-06-11 17:58:49 +0200

    Added support for arrays of requests in totals/clients API calls.

--- src/controller/controller_statistics.cpp
+++ src/controller/controller_statistics.cpp
@@ -104,6 +104,13 @@ bool Controller::hasViewers(std::string streamName){
 ///   "time": 1234567
 /// }
 /// ~~~~~~~~~~~~~~~
+/// OR
+/// ~~~~~~~~~~~~~~~{.js}
+/// [
+///   {},//request object as above
+///   {}//repeat the structure as many times as wanted
+/// ]
+/// ~~~~~~~~~~~~~~~
 /// and are responded to as:
 /// ~~~~~~~~~~~~~~~{.js}
 /// {
@@ -115,6 +122,7 @@ bool Controller::hasViewers(std::string streamName){
 ///   "data": [[x, y, z], [x, y, z], [x, y, z]]
 /// }
 /// ~~~~~~~~~~~~~~~
+/// In case of the second method, the response is an array in the same order as the requests.
 void Controller::fillClients(JSON::Value & req, JSON::Value & rep){
   //first, figure out the timestamp wanted
   long long int reqTime = 0;
@@ -288,6 +296,13 @@ class totalsData {
 ///   "end": 1234567
 /// }
 /// ~~~~~~~~~~~~~~~
+/// OR
+/// ~~~~~~~~~~~~~~~{.js}
+/// [
+///   {},//request object as above
+///   {}//repeat the structure as many times as wanted
+/// ]
+/// ~~~~~~~~~~~~~~~
 /// and are responded to as:
 /// ~~~~~~~~~~~~~~~{.js}
 /// {
@@ -303,6 +318,7 @@ class totalsData {
 ///   "data": [[x, y, z], [x, y, z], [x, y, z]]
 /// }
 /// ~~~~~~~~~~~~~~~
+/// In case of the second method, the response is an array in the same order as the requests.
 void Controller::fillTotals(JSON::Value & req, JSON::Value & rep){
   //first, figure out the timestamps wanted
   long long int reqStart = 0;

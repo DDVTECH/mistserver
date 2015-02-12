bbe907575c39e5034a9b5e35635de7fc31de1124
Thulinma <jaron@vietors.com>
2015-02-12 15:57:46 +0100

    Improvements to the TS analyser by Erik Zandvliet - TS push output updated to latest lib.

--- src/analysers/ts_analyser.cpp
+++ src/analysers/ts_analyser.cpp
@@ -8,16 +8,63 @@
 #include <string.h>
 #include <fstream>
 #include <unistd.h>
+#include <sstream>
 #include <signal.h>
 #include <mist/ts_packet.h>
 #include <mist/config.h>
 
 
 namespace Analysers {
+  std::string printPES(const std::string & pesPacket){
+    std::stringstream res;
+    res << "PES Packet" << std::endl;
+    res << "  Packet Start Code Prefix: "
+      << std::hex << std::setw(2) << std::setfill('0')
+      << (int)pesPacket[0] << (int)pesPacket[1] << (int)pesPacket[2];
+    res << std::endl;
+    res << "  Stream Id: "
+      << std::hex << std::setw(2) << std::setfill('0')
+      << (int)pesPacket[3];
+    res << std::endl;
+    res << "  Packet Length: " << (((int)pesPacket[4]) << 8 | pesPacket[5]) << std::endl;
+    res << "  PES Scrambling Control: " << (int)((pesPacket[6] & 0x30) >> 4) << std::endl;
+    res << "  PES Priority: " << (int)((pesPacket[6] & 0x08) >> 3) << std::endl;
+    res << "  Data Alginment Indicator: " << (int)((pesPacket[6] & 0x04) >> 2) << std::endl;
+    res << "  Copyright: " << (int)((pesPacket[6] & 0x02) >> 1) << std::endl;
+    res << "  Original or Copy: " << (int)((pesPacket[6] & 0x01)) << std::endl;
+    int timeFlags = ((pesPacket[7] & 0xC0) >> 6);
+    res << "  PTS/DTS Flags: " << timeFlags << std::endl;
+    res << "  ESCR Flag: " << (int)((pesPacket[7] & 0x20) >> 5) << std::endl;
+    res << "  ES Rate Flag: " << (int)((pesPacket[6] & 0x10) >> 4) << std::endl;
+    res << "  DSM Trick Mode Flag: " << (int)((pesPacket[6] & 0x08) >> 3) << std::endl;
+    res << "  Additional Copy Info Flag: " << (int)((pesPacket[6] & 0x04) >> 2) << std::endl;
+    res << "  PES CRC Flag: " << (int)((pesPacket[6] & 0x02) >> 1) << std::endl;
+    res << "  PES Extension Flag: " << (int)((pesPacket[6] & 0x01)) << std::endl;
+    res << "  PES Header Data Length: " << (int)pesPacket[7] << std::endl;
+    if (timeFlags & 0x02){
+      long long unsigned int time = ((pesPacket[8] >> 1) & 0x07);
+      time <<= 15;
+      time |= ((int)pesPacket[9] << 7) | ((pesPacket[10] >> 1) & 0x7F);
+      time <<= 15;
+      time |= ((int)pesPacket[11] << 7) | ((pesPacket[12] >> 1) & 0x7F);
+      res << "  PTS: " << std::dec << time << std::endl;
+    }
+    if (timeFlags & 0x01){
+      long long unsigned int time = ((pesPacket[13] >> 1) & 0x07);
+      time <<= 15;
+      time |= ((int)pesPacket[14] << 7) | (pesPacket[15] >> 1);
+      time <<= 15;
+      time |= ((int)pesPacket[16] << 7) | (pesPacket[17] >> 1);
+      res << "  DTS: " << std::dec << time << std::endl;
+    }
+    return res.str();
+  }
+
   /// Debugging tool for TS data.
   /// Expects TS data through stdin, outputs human-readable information to stderr.
   /// \return The return code of the analyser.
   int analyseTS(bool validate, bool analyse){
+    std::map<unsigned long long, std::string> payloads;
     TS::Packet packet;
     long long int upTime = Util::bootSecs();
     int64_t pcr = 0;
@@ -28,7 +75,14 @@ namespace Analysers {
       if(std::cin.gcount() != 188){break;}
       bytes += 188;
       if(packet.FromPointer(packetPtr)){
-        if(analyse){std::cout << packet.toPrettyString();}
+        if(analyse){
+          std::cout << packet.toPrettyString();
+          if (packet.UnitStart() && payloads[packet.PID()] != ""){
+            std::cout << printPES(payloads[packet.PID()]);
+            payloads.erase(packet.PID());
+          }
+          payloads[packet.PID()].append(packet.getPayload(), packet.getPayloadLength());
+        }
         if(packet.getSyncByte() == 0x47 && packet.AdaptationField() > 1 && packet.PCRFlag()){pcr = packet.PCR();}
       }
       if(bytes > 1024){
@@ -39,6 +93,11 @@ namespace Analysers {
         }
         bytes = 0;
       }
+    }
+    for (std::map<unsigned long long, std::string>::iterator it = payloads.begin(); it != payloads.end(); it++){
+      if (!it->first || it->first == 4096){ continue; }
+      std::cout << "Remainder of a packet on track " << it->first << ":" << std::endl;
+      std::cout << printPES(it->second);
     }
     long long int finTime = Util::bootSecs();
     if(validate){

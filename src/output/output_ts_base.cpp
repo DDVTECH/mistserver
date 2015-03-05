ff049f4ea24beffab38144ad71999f115b22691e
wouter spruit <wouter.spruit@ddvtech.com>
2015-03-05 13:39:31 +0100

    continuity improvements

--- src/output/output_ts_base.cpp
+++ src/output/output_ts_base.cpp
@@ -17,9 +17,9 @@ namespace Mist {
       if ( (sendRepeatingHeaders && packCounter % 42 == 0) || !packCounter){
         TS::Packet tmpPack;
         tmpPack.FromPointer(TS::PAT);
-        tmpPack.setContinuityCounter(++contCounters[tmpPack.getPID()]);
+        tmpPack.setContinuityCounter(++contCounters[0]);
         sendTS(tmpPack.checkAndGetBuffer());
-        sendTS(TS::createPMT(selectedTracks, myMeta, ++contCounters[tmpPack.getPID()]));
+        sendTS(TS::createPMT(selectedTracks, myMeta, ++contCounters[4096]));
         packCounter += 2;
       }
       sendTS(packData.checkAndGetBuffer());
@@ -35,6 +35,7 @@ namespace Mist {
       packData.setContinuityCounter(++contCounters[packData.getPID()]);
       if (first[currentPacket.getTrackId()]){
         packData.setUnitStart(1);
+        packData.setDiscontinuity(true);
         if (myMeta.tracks[currentPacket.getTrackId()].type == "video"){
           if (currentPacket.getInt("keyframe")){
             packData.setRandomAccess(1);

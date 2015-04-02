3f6e465d6edbb38bfd1f69d6c5e01b8c2327197f
Erik Zandvliet <erik.zandvliet@ddvtech.com>
2015-04-02 15:34:51 +0200

    LSP is now pre-minified, added a script to generate minified version, and updated the makefile accordingly

--- lsp/generateLSP.sh
+++ lsp/generateLSP.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-cat header.html > $1
-echo "<script>" >> $1
-cat plugins/jquery.js plugins/jquery.flot.min.js plugins/jquery.flot.time.min.js plugins/jquery.qrcode.min.js >> $1
-cat plugins/md5.js plugins/cattablesort.js mist.js >> $1
-echo "</script><style>" >> $1
-cat main.css >> $1
-echo "</style>" >> $1
-cat footer.html >> $1
-
+java -jar closure-compiler.jar --warning_level QUIET  plugins/md5.js plugins/cattablesort.js mist.js > minified.js

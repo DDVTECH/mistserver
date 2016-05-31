e7962fce26dd76c295a89d449cbd2a538f905207
Thulinma <jaron@vietors.com>
2016-05-31 10:11:38 +0200

    Fixed IPv6 addresses in hostnames in info_ json_ and embed_ URLs as well as .smil URLs.

--- src/output/output_http_internal.cpp
+++ src/output/output_http_internal.cpp
@@ -266,8 +266,8 @@ namespace Mist {
     // send smil MBR index
     if (H.url.length() > 6 && H.url.substr(H.url.length() - 5, 5) == ".smil"){
       std::string host = H.GetHeader("Host");
-      if (host.find(':')){
-        host.resize(host.find(':'));
+      if (host.rfind(':') != std::string::npos && *host.rbegin() != ']'){
+        host.resize(host.rfind(':'));
       }
       
       std::string port, url_rel;
@@ -326,8 +326,8 @@ namespace Mist {
       std::string response;
       std::string rURL = H.url;
       std::string host = H.GetHeader("Host");
-      if (host.find(':') != std::string::npos){
-        host.resize(host.find(':'));
+      if (host.rfind(':') != std::string::npos && *host.rbegin() != ']'){
+        host.resize(host.rfind(':'));
       }
       H.Clean();
       H.SetHeader("Server", "MistServer/" PACKAGE_VERSION);

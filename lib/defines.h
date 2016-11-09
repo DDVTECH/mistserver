19b67e4551335785bc30020d5f9afc62867eceff
Thulinma <jaron@vietors.com>
2016-11-09 12:38:05 +0100

    Updated default live metadata page size to 16MiB, to allow for bigger streams

--- lib/defines.h
+++ lib/defines.h
@@ -71,11 +71,8 @@ static const char * DBG_LVL_LIST[] = {"NONE", "FAIL", "ERROR", "WARN", "INFO", "
 #define INPUT_TIMEOUT STATS_DELAY
 #endif
 
-/// The size used for stream header pages under Windows, where they cannot be size-detected.
-#define DEFAULT_META_PAGE_SIZE 16 * 1024 * 1024
-
-/// The size used for stream header pages under Windows, where they cannot be size-detected.
-#define DEFAULT_STRM_PAGE_SIZE 4 * 1024 * 1024
+/// The size used for stream headers for live streams
+#define DEFAULT_STRM_PAGE_SIZE 16 * 1024 * 1024
 
 /// The size used for stream data pages under Windows, where they cannot be size-detected.
 #define DEFAULT_DATA_PAGE_SIZE SHM_DATASIZE * 1024 * 1024

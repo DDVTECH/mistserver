707c055ba5734ee9c4174b84de7a5db149babfa8
Thulinma <jaron@vietors.com>
2017-01-02 12:08:58 +0100

    RelAccX test analyser

---
+++ src/analysers/rax_analyser.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <mist/util.h>
+#include <mist/shared_memory.h>
+
+int main(int argc, char ** argv){
+  if (argc < 1){
+    FAIL_MSG("Usage: %s MEMORY_PAGE_NAME");
+    return 1;
+  }
+  IPC::sharedPage f(argv[1], 0, false, false);
+  const Util::RelAccX A(f.mapped, false);
+  if (A.isReady()){
+    std::cout << A.toPrettyString() << std::endl;
+  }else{
+    std::cout << "Memory structure " << argv[1] << " is not ready" << std::endl;
+  }
+}
+

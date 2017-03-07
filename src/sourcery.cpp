907d48b7e50ccb85765d4f9f3b418d3c098e6f4d
Thulinma <jaron@vietors.com>
2017-03-07 17:00:49 +0100

    Fix CMake problem with headers, fixed sourcery cross compile detection

--- src/sourcery.cpp
+++ src/sourcery.cpp
@@ -8,7 +8,7 @@
 int main(int argc, char* argv[]){
   if (argc < 4) {
     std::cerr << "Usage: " << argv[0] << " <inputFile> <variableName> <outputFile>" << std::endl;
-    return 1;
+    return 2;
   }
   std::ofstream tmp(argv[3]);
   //begin the first line

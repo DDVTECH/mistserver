f8e9904e4e7be252c2d23f1d0e97ee34237ec097
Thulinma <jaron@vietors.com>
2018-07-18 21:37:59 +0200

    Added Opus::getPreSkip function

--- lib/opus.h
+++ lib/opus.h
@@ -1,6 +1,7 @@
 #include <string>
 
 namespace Opus{
+  uint16_t getPreSkip(const char * initData);
   unsigned int Opus_getDuration(const char *part);
   std::string Opus_prettyPacket(const char *part, int len);
 }

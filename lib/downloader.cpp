8dd403a098e8c4fec7effd46adb4b89688fa13a6
Thulinma <jaron@vietors.com>
2019-11-07 13:40:35 +0100

    Added ability to check if the downloader is being proxied or not

--- lib/downloader.cpp
+++ lib/downloader.cpp
@@ -20,6 +20,8 @@ namespace HTTP{
     }
   }
 
+  bool Downloader::isProxied() const{return proxied;}
+
   /// Returns a reference to the internal HTTP::Parser body element
   std::string &Downloader::data(){return H.body;}
 

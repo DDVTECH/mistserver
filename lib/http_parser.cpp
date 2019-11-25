8ce3498269da78bda91c3a3984dde8c734544542
Thulinma <jaron@vietors.com>
2019-11-25 11:29:16 +0100

    Optimized HTTP parser to not attempt POST variable parsing on _all_ body payloads, but only POSTed payloads

--- lib/http_parser.cpp
+++ lib/http_parser.cpp
@@ -875,7 +875,8 @@ bool HTTP::Parser::parse(std::string &HTTPbuffer){
           HTTPbuffer.erase(0, toappend);
         }
         if (length == body.length()){
-          parseVars(body, vars); // parse POST variables
+          //parse POST variables
+          if (method == "POST"){parseVars(body, vars);}
           return true;
         }else{
           return false;

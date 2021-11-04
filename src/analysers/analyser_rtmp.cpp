ab3e03fa9845c57e9df54405150a20f58d345fde
Balder <balder.vietor@ddvtech.com>
2021-11-04 17:11:26 +0100

    edits from Thul to get RTMP working

--- src/analysers/analyser_rtmp.cpp
+++ src/analysers/analyser_rtmp.cpp
@@ -260,140 +260,142 @@ bool AnalyserRTMP::parsePacket(){
   size_t bytesNeeded = 1024;
   std::string tmpBuf;
 
-  // Keep trying until the buffer is parseable
-  while (!next.Parse(strbuf)) {
-    tmpBuf = removeFromBufferBlocking(bytesNeeded);
-    // If EOF is reached, whatever is left in the buffer might be parseable
-    if (!isOpen()){
-      if (!tmpBuf.size()){
-        FAIL_MSG("Connection lost or EOF reached. Quitting...");
-        return false;
+  while(true){
+    // Keep trying until the buffer is parseable
+    while (!next.Parse(strbuf)) {
+      tmpBuf = removeFromBufferBlocking(bytesNeeded);
+      // If EOF is reached, whatever is left in the buffer might be parseable
+      if (!isOpen()){
+        if (!tmpBuf.size()){
+          FAIL_MSG("Connection lost or EOF reached. Quitting...");
+          return false;
+        }
       }
+      mediaDown += bytesNeeded;
+      strbuf.append(tmpBuf);
+      tmpBuf.clear();
     }
-    mediaDown += bytesNeeded;
-    strbuf.append(tmpBuf);
-    tmpBuf.clear();
-  }
 
-  // We now know for sure that we've parsed a packet
-  DETAIL_HI("Chunk info: [%#2X] CS ID %u, timestamp %" PRIu64 ", len %u, type ID %u, Stream ID %u",
-            next.headertype, next.cs_id, next.timestamp, next.len, next.msg_type_id, next.msg_stream_id);
-  switch (next.msg_type_id){
-  case 0: // does not exist
-    DETAIL_LOW("Error chunk @ %zu - CS%u, T%" PRIu64 ", L%i, LL%i, MID%i", read_in - strbuf.size(),
-               next.cs_id, next.timestamp, next.real_len, next.len_left, next.msg_stream_id);
-    return 0;
-    break; // happens when connection breaks unexpectedly
-  case 1:  // set chunk size
-    RTMPStream::chunk_rec_max = ntohl(*(int *)next.data.c_str());
-    DETAIL_MED("CTRL: Set chunk size: %zu", RTMPStream::chunk_rec_max);
-    break;
-  case 2: // abort message - we ignore this one
-    DETAIL_MED("CTRL: Abort message: %i", ntohl(*(int *)next.data.c_str()));
-    // 4 bytes of stream id to drop
-    break;
-  case 3: // ack
-    RTMPStream::snd_window_at = Bit::btohl(next.data.data());
-    DETAIL_MED("CTRL: Acknowledgement: %zu", RTMPStream::snd_window_at);
-    break;
-  case 4:{
-    short int ucmtype = ntohs(*(short int *)next.data.c_str());
-    switch (ucmtype){
-    case 0:
-      DETAIL_MED("CTRL: User control message: stream begin %u",
-                 ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+    // We now know for sure that we've parsed a packet
+    DETAIL_HI("Chunk info: [%#2X] CS ID %u, timestamp %" PRIu64 ", len %u, type ID %u, Stream ID %u",
+              next.headertype, next.cs_id, next.timestamp, next.len, next.msg_type_id,
+              next.msg_stream_id);
+    switch (next.msg_type_id){
+    case 0: // does not exist
+      DETAIL_LOW("Error chunk @ %" PRIu64 " - CS%i, T%" PRIu64 ", L%i, LL%i, MID%i", mediaDown - strbuf.size(),
+                 next.cs_id, next.timestamp, next.real_len, next.len_left, next.msg_stream_id);
+      return 0;
+      break; // happens when connection breaks unexpectedly
+    case 1:  // set chunk size
+      RTMPStream::chunk_rec_max = ntohl(*(int *)next.data.c_str());
+      DETAIL_MED("CTRL: Set chunk size: %lu", RTMPStream::chunk_rec_max);
       break;
-    case 1:
-      DETAIL_MED("CTRL: User control message: stream EOF %u",
-                 ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+    case 2: // abort message - we ignore this one
+      DETAIL_MED("CTRL: Abort message: %i", ntohl(*(int *)next.data.c_str()));
+      // 4 bytes of stream id to drop
       break;
-    case 2:
-      DETAIL_MED("CTRL: User control message: stream dry %u",
-                 ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+    case 3: // ack
+      RTMPStream::snd_window_at = ntohl(*(int *)next.data.c_str());
+      DETAIL_MED("CTRL: Acknowledgement: %lu", RTMPStream::snd_window_at);
       break;
-    case 3:
-      DETAIL_MED("CTRL: User control message: setbufferlen %u",
-                 ntohl(*(unsigned int *)(next.data.c_str() + 2)));
-      break;
-    case 4:
-      DETAIL_MED("CTRL: User control message: streamisrecorded %u",
-                 ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+    case 4:{
+      short int ucmtype = ntohs(*(short int *)next.data.c_str());
+      switch (ucmtype){
+      case 0:
+        DETAIL_MED("CTRL: User control message: stream begin %u",
+                   ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+        break;
+      case 1:
+        DETAIL_MED("CTRL: User control message: stream EOF %u",
+                   ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+        break;
+      case 2:
+        DETAIL_MED("CTRL: User control message: stream dry %u",
+                   ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+        break;
+      case 3:
+        DETAIL_MED("CTRL: User control message: setbufferlen %u",
+                   ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+        break;
+      case 4:
+        DETAIL_MED("CTRL: User control message: streamisrecorded %u",
+                   ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+        break;
+      case 6:
+        DETAIL_MED("CTRL: User control message: pingrequest %u",
+                   ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+        break;
+      case 7:
+        DETAIL_MED("CTRL: User control message: pingresponse %u",
+                   ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+        break;
+      case 31:
+      case 32:
+        // don't know, but not interesting anyway
+        break;
+      default:
+        DETAIL_LOW("CTRL: User control message: UNKNOWN %hu - %u", ucmtype,
+                   ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+        break;
+      }
+    }break;
+    case 5: // window size of other end
+      RTMPStream::rec_window_size = ntohl(*(int *)next.data.c_str());
+      RTMPStream::rec_window_at = RTMPStream::rec_cnt;
+      DETAIL_MED("CTRL: Window size: %lu", RTMPStream::rec_window_size);
       break;
     case 6:
-      DETAIL_MED("CTRL: User control message: pingrequest %u",
-                 ntohl(*(unsigned int *)(next.data.c_str() + 2)));
+      RTMPStream::snd_window_size = ntohl(*(int *)next.data.c_str());
+      // 4 bytes window size, 1 byte limit type (ignored)
+      DETAIL_MED("CTRL: Set peer bandwidth: %lu", RTMPStream::snd_window_size);
       break;
-    case 7:
-      DETAIL_MED("CTRL: User control message: pingresponse %u",
-                 ntohl(*(unsigned int *)(next.data.c_str() + 2)));
-      break;
-    case 31:
-    case 32:
-      // don't know, but not interesting anyway
-      break;
-    default:
-      DETAIL_LOW("CTRL: User control message: UNKNOWN %hu - %u", ucmtype,
-                 ntohl(*(unsigned int *)(next.data.c_str() + 2)));
-      break;
-    }
-  }break;
-  case 5: // window size of other end
-    RTMPStream::rec_window_size = ntohl(*(int *)next.data.c_str());
-    RTMPStream::rec_window_at = RTMPStream::rec_cnt;
-    DETAIL_MED("CTRL: Window size: %zu", RTMPStream::rec_window_size);
-    break;
-  case 6:
-    RTMPStream::snd_window_size = ntohl(*(int *)next.data.c_str());
-    // 4 bytes window size, 1 byte limit type (ignored)
-    DETAIL_MED("CTRL: Set peer bandwidth: %zu", RTMPStream::snd_window_size);
-    break;
-  case 8:
-  case 9:
-    if (detail >= 4 || reconstruct.good() || validate){
-      F.ChunkLoader(next);
-      mediaTime = F.tagTime();
-      DETAIL_VHI("[%lu+%lu] %s", F.tagTime(), F.offset(), F.tagType().c_str());
-      if (reconstruct.good()){reconstruct.write(F.data, F.len);}
-    }
-    break;
-  case 15: DETAIL_MED("Received AFM3 data message"); break;
-  case 16: DETAIL_MED("Received AFM3 shared object"); break;
-  case 17:{
-    DETAIL_MED("Received AFM3 command message:");
-    char soort = next.data[0];
-    next.data = next.data.substr(1);
-    if (soort == 0){
+    case 8:
+    case 9:
+      if (detail >= 4 || reconstruct.good() || validate){
+        F.ChunkLoader(next);
+        if (F.tagTime() > mediaTime){mediaTime = F.tagTime();}
+        DETAIL_VHI("[%lu+%lu] %s", F.tagTime(), F.offset(), F.tagType().c_str());
+        if (reconstruct.good()){reconstruct.write(F.data, F.len);}
+      }
+      return true;
+    case 15: DETAIL_MED("Received AFM3 data message"); break;
+    case 16: DETAIL_MED("Received AFM3 shared object"); break;
+    case 17:{
+      DETAIL_MED("Received AFM3 command message:");
+      char soort = next.data[0];
+      next.data = next.data.substr(1);
+      if (soort == 0){
+        amfdata = AMF::parse(next.data);
+        DETAIL_MED("%s", amfdata.Print().c_str());
+      }else{
+        amf3data = AMF::parse3(next.data);
+        DETAIL_MED("%s", amf3data.Print().c_str());
+      }
+    }break;
+    case 18:{
+      DETAIL_MED("Received AFM0 data message (metadata):");
       amfdata = AMF::parse(next.data);
       DETAIL_MED("%s", amfdata.Print().c_str());
-    }else{
-      amf3data = AMF::parse3(next.data);
-      DETAIL_MED("%s", amf3data.Print().c_str());
-    }
-  }break;
-  case 18:{
-    DETAIL_MED("Received AFM0 data message (metadata):");
-    amfdata = AMF::parse(next.data);
-    DETAIL_MED("%s", amfdata.Print().c_str());
-    if (reconstruct.good()){
-      F.ChunkLoader(next);
-      reconstruct.write(F.data, F.len);
-    }
-  }break;
-  case 19: DETAIL_MED("Received AFM0 shared object"); break;
-  case 20:{// AMF0 command message
-    DETAIL_MED("Received AFM0 command message:");
-    amfdata = AMF::parse(next.data);
-    DETAIL_MED("%s", amfdata.Print().c_str());
-  }break;
-  case 22:
-    if (reconstruct.good()){reconstruct << next.data;}
-    break;
-  default:
-    FAIL_MSG(
-        "Unknown chunk received! Probably protocol corruption, stopping parsing of incoming data.");
-    return false;
-    break;
-  }// switch for type of chunk
-
+      if (reconstruct.good()){
+        F.ChunkLoader(next);
+        reconstruct.write(F.data, F.len);
+      }
+    }break;
+    case 19: DETAIL_MED("Received AFM0 shared object"); break;
+    case 20:{// AMF0 command message
+      DETAIL_MED("Received AFM0 command message:");
+      amfdata = AMF::parse(next.data);
+      DETAIL_MED("%s", amfdata.Print().c_str());
+    }break;
+    case 22:
+      if (reconstruct.good()){reconstruct << next.data;}
+      break;
+    default:
+      FAIL_MSG(
+          "Unknown chunk received! Probably protocol corruption, stopping parsing of incoming data.");
+      return false;
+      break;
+    }// switch for type of chunk
+  }
   return true;
 }

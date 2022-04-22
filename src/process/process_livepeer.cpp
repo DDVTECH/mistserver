887aa4eaaf8878d7e93e9a007e3b20ae5912c9c4
Eli Mallon <eli@livepeer.org>
2022-04-22 10:48:45 +0000

    MistProcLivepeer: fix access_token check

--- src/process/process_livepeer.cpp
+++ src/process/process_livepeer.cpp
@@ -888,7 +888,7 @@ int main(int argc, char *argv[]){
     dl.setHeader("Authorization", "Bearer "+Mist::opt["access_token"].asStringRef());
   }
 
-  if (!(!Mist::opt.isMember("hardcoded_broadcasters") || !Mist::opt["hardcoded_broadcasters"] || !Mist::opt["hardcoded_broadcasters"].isString())){
+  if (Mist::opt.isMember("hardcoded_broadcasters") && Mist::opt["hardcoded_broadcasters"] && Mist::opt["hardcoded_broadcasters"].isString()){
     Mist::lpBroad = JSON::fromString(Mist::opt["hardcoded_broadcasters"].asStringRef());
   } else {
     //Get broadcaster list, pick first valid address
@@ -908,8 +908,7 @@ int main(int argc, char *argv[]){
     return 1;
   }
   INFO_MSG("Using broadcaster: %s", Mist::currBroadAddr.c_str());
-
-  if (Mist::opt["access_token"].size()){
+  if (Mist::opt.isMember("access_token") && Mist::opt["access_token"] && Mist::opt["access_token"].isString()){
     //send transcode request
     dl.setHeader("Content-Type", "application/json");
     dl.setHeader("Authorization", "Bearer "+Mist::opt["access_token"].asStringRef());

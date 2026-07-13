5e2ff81dca501ae09b8ca6bb687b02a42b88c49c
Thulinma <jaron@vietors.com>
2026-07-13 09:36:19 +0200

    Fix TS-SRT output compile on MacOS

--- src/output/output_tssrt.h
+++ src/output/output_tssrt.h
@@ -32,7 +32,7 @@ namespace Mist{
     bool pushOut;
     bool rawMode{false};
     uint64_t lastRawPacket{0};
-    uint64_t rawIdx{INVALID_TRACK_ID};
+    size_t rawIdx{INVALID_TRACK_ID};
     Util::ResizeablePointer packetBuffer;
     TS::Stream tsIn;
     TS::Assembler assembler;
